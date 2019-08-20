76bbbfb612d65aabaea44d1c9de5c8a532ff16ae
JlFerrete <joseluisferrete@gmail.com>
2019-08-20 19:51:09 +0200

    Added popular top5 list

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ const API_KEY = "1415cd15b1c28a6b42679e0f8b7b9d30";
 
 document.addEventListener("DOMContentLoaded", () => {
     renderNewMovies();
+    renderPopularMovies();
 })
 
 const getNewMovies = () => {
@@ -51,4 +52,39 @@ const renderNewMovies = async () => {
 
     document.getElementsByClassName('list-news-movies')[0].innerHTML = html;
 
+}
+
+const getPopularMovies = () => {
+    const url = `${URL_PATH}/3/movie/popular?api_key=${API_KEY}&language=es-ES&page=1`;
+
+    return fetch(url)
+        .then(response => response.json())
+        .then(result => result.results)
+        .catch(error => console.log(error))
+}
+
+const renderPopularMovies = async () => {
+    const movies = await getPopularMovies();
+
+    let html = ``;
+    movies.forEach((movie, index) => {
+        console.log(movie);
+
+        const { id, title, poster_path } = movie;
+        const movieCover = `https://image.tmdb.org/t/p/original${poster_path}`;
+        const urlMovie = `../movie.html?id=${id}`;
+
+        if (index < 5) {
+            html +=
+                `<li class="list-group-item">
+                    <img src="${movieCover}" alt="${title}">
+                    <h3>${title}</h3>
+                    <a href="${urlMovie}" class="btn btn-primary">Ver Más</a>
+                </li>`
+                ;
+        }
+
+        document.getElementsByClassName('now-playing__list')[0].innerHTML = html;
+
+    })
 }

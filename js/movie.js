a1a46a4c0ec7901a39a9b6c84af4d00c09f9c831
JlFerrete <joseluisferrete@gmail.com>
2019-08-21 23:49:36 +0200

    Added teaser into modal

--- js/movie.js
+++ js/movie.js
@@ -32,7 +32,7 @@ const renderMovieDetails = async (movieId) => {
     renderBackground(backdrop_path);
     renderPoster(poster_path, title);
     renderMovieData(title, overview, genres, release_date);
-    // getTeaser(movieId);
+    getTeaser(movieId);
 }
 
 const renderBackground = (backdrop_path) => {
@@ -71,5 +71,36 @@ const renderMovieData = (title, overview, genres, relese_date) => {
         </ul>
     `;
     document.getElementsByClassName('movie-info__data')[0].innerHTML = html;
+}
+
+const getTeaser = (movieId) => {
+    const url = `${URL_PATH}/3/movie/${movieId}/videos?api_key=${API_KEY}&language=es-ES`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(result => { renderTeaser(result) })
+        .catch(error => console.log(error));
+}
+
+const renderTeaser = (objVideo) => {
+    let keyVideo = "";
+    objVideo.results.forEach(video => {
+        if (video.type === "Teaser" && video.site === "YouTube") {
+            keyVideo = video.key;
+        }
+    });
+
+    let urlIframe = "";
+    if (keyVideo !== "") {
+        urlIframe = `
+            <iframe width="100%" height="440px" src="https://www.youtube.com/embed/${keyVideo}"
+            frameborder="0" allow="accelerometer"; autoplay; ecrypted-media;
+            gryscope; picture-in-pincture"allowfullscreen></iframe>
+        `;
+    } else {
+        urlIframe = "<div class='no-teaser'>La pelicula no tiene trailer</div>";
+    }
+    document.getElementsByClassName('video-teaser-iframe')[0].innerHTML = urlIframe;
+
 }
 

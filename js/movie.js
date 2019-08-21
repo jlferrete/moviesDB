2ebf60ca047c52b1d04f5164d20570f3125fc19c
JlFerrete <joseluisferrete@gmail.com>
2019-08-21 21:20:58 +0200

    Getting data from movie by ID

--- js/movie.js
+++ js/movie.js
@@ -4,6 +4,7 @@ let MOVIE_ID = "";
 
 document.addEventListener("DOMContentLoaded", () => {
     MOVIE_ID = getUrlVars().id;
+    renderMovieDetails(MOVIE_ID);
 });
 
 const getUrlVars = () => {
@@ -14,3 +15,22 @@ const getUrlVars = () => {
     return vars;
 }
 
+const getMovieDetails = (movieId) => {
+    const url = `${URL_PATH}/3/movie/${movieId}?api_key=${API_KEY}&language=es-ES`;
+
+    return fetch(url)
+        .then(response => response.json())
+        .then(result => result)
+        .catch(error => console.log(error))
+}
+
+const renderMovieDetails = async (movieId) => {
+    const movieDetails = await getMovieDetails(movieId);
+    console.log(movieDetails);
+
+    // const { backdrop_path, poster_path, title, overview, genres, release_date } = movieDetails;
+    // renderBackground(backdrop_path);
+    // renderPoster(poster_path, title);
+    // renderMovieData(title, overview, genres, release_date);
+    // getTeaser(movieId);
+}
